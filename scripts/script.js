12b2ac22113f94f2c2fdb4c056a15825a675fb97
José Luiz <jlz.dev@outlook.com>
2024-07-03 18:35:19 -0300

    site update

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 var popupViews = document.querySelectorAll('.popup-view');
 var popupBtns = document.querySelectorAll('.popup-btn');
 var closeBtns = document.querySelectorAll('.close-btn');
+ var body = document.body;
+
 
 
 // javascript for quick view button
 var popup = function(popupClick){
     popupViews[popupClick].classList.add('active');
+    body.classList.add('noscroll');
+    // popupViews[popupClick].style.overflowY= "scroll";
 }
 
 popupBtns.forEach((popupBtn, i) => {
@@ -19,9 +23,27 @@ closeBtns.forEach((closeBtn) => {
     closeBtn.addEventListener("click", () => {
         popupViews.forEach((popupView) => {
             popupView.classList.remove('active');
+            popupView.style.overflowY= "hidden";
+            body.classList.remove('noscroll');
         });
     });
 });
+
+// javascript for close btn
+
+
+// Adiciona um listener de evento para cada 'popup-view'
+popupViews.forEach((popupView) => {
+    popupView.addEventListener("click", () => {
+        // Remove a classe 'active' e ajusta o estilo apenas para o popup específico clicado
+        popupView.classList.remove('active');
+        popupView.style.overflowY = "hidden";
+        
+        // Remove a classe 'noscroll' do corpo
+        document.body.classList.remove('noscroll');
+    });
+});
+
 
 
 
